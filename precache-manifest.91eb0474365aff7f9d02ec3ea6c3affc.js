3663c652e2f3ed261b9e7abbd22eb5e03d4e42a5
LopezJhon <jhon_l_24@hotmail.com>
2020-01-28 17:19:23 -0500

    Updates

---
+++ precache-manifest.91eb0474365aff7f9d02ec3ea6c3affc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77630fc7926eea3c9465b97cd8c45b7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "95a8e6313b55a7095ede",
+    "url": "/monsters-rolodex/static/css/main.1f752133.chunk.css"
+  },
+  {
+    "revision": "cd1cf9dbc2dd29055e19",
+    "url": "/monsters-rolodex/static/js/2.044ff411.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.044ff411.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95a8e6313b55a7095ede",
+    "url": "/monsters-rolodex/static/js/main.8b1079ea.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
